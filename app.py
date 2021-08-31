a79fbae880d4673ec392d44f2e072ff78f779daf
finnsterfran <j.frannie.yip@gmail.com>
2021-08-31 10:18:30 +0000

    Commit 2: create home.html, implemented html for base.html, add styling in style.css

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
-from flask import Flask 
+from flask import (
+    Flask, request, render_template,
+    redirect, flash, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -11,11 +14,11 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 @app.route("/")
-def test():
-    return "This works fine."
+def home():
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
